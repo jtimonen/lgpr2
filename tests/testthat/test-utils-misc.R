cca17a9dd083641eec622090993244a49b90b59e
Juho Timonen <juho.timonen@aalto.fi>
2024-05-09 14:34:00 +0300

    fix test

--- tests/testthat/test-utils-misc.R
+++ tests/testthat/test-utils-misc.R
@@ -1,4 +1,4 @@
 test_that("stanmodel_functions works", {
-  sm <- stanmodel_functions("hazard/integrate")
+  sm <- stanmodel_functions("gp/basisfun")
   expect_true(inherits(sm, "CmdStanModel"))
 })
